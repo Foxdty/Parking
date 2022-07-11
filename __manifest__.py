4a3e7136adb41f5f6cb3c66604ae7862669a9116
Foxdty <nguyenvanthanh0055@gmail.com>
2022-07-11 09:33:47 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Parking',
+    'version': '1.2',
+    'category': 'Sales/Parking',
+    'sequence': 1,
+    'summary': ' opportunities',
+    'description': "",
+    'license': 'OEEL-1',
+    'website': '',
+    'depends': [
+        'base',
+        
+    ],
+    
+    'data': [
+ 
+    ],
+    "demo": [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True
+}
---
+++ models/parking_lot.py
@@ -0,0 +1,6 @@
+from odoo import fields,models
+
+class parking_lot(models.Model):
+    _name='parking.lot'
+    _description='lot'
+    
